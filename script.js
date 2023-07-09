75c6a52ecadff49c071912f12e8789d7dfab8325
iftekar2 <iftekar.hossain123@gmail.com>
2023-07-08 22:27:46 -0400

    Allowing the users to click Submit Button and adding the destination dynamically

--- script.js
+++ script.js
@@ -3,21 +3,47 @@ const submitButton = document.getElementById("button");
 submitButton.addEventListener("click", showUserInfo);
 
 function showUserInfo() {
-  //This is getting the User Information
+  // Get user input values
   const destination = document.getElementById("destination").value;
   const city = document.getElementById("city").value;
   const image = document.getElementById("image").value;
   const description = document.getElementById("description").value;
 
-  const destinationInput = document.getElementById("destinationInput");
-  destinationInput.innerHTML = destination;
+  showInfo(destination, city, image, description);
+}
 
-  const cityInput = document.getElementById("cityInput");
-  cityInput.innerHTML = city;
+function showInfo(destination, city, image, description) {
+  const show = document.getElementById("show");
+  const newShow = `
+  <section class="showUserInputSection">
+    <div class="showUserInput">
+      <div class="destination">
+        <h3>Destination Name</h3>
+        <p id="destinationInput">${destination}</p>
+      </div>
 
-  const imageInput = document.getElementById("imageInput");
-  imageInput.src = image;
+      <div class="city">
+        <h3>City Name</h3>
+        <p id="cityInput">${city}</p>
+      </div>
 
-  const descriptionInput = document.getElementById("descriptionInput");
-  descriptionInput.innerHTML = description;
+      <div class="image">
+        <h3>Image</h3>
+        <div class="imageDiv">
+            <img id="imageInput" src="${image}"></img>
+        </div>
+      </div>
+
+      <div class="description">
+        <h3>City Description</h3>
+        <p id="descriptionInput">${description}</p>
+      </div>
+    </div>
+  </section>`;
+  // Create a new container element
+  const newContainer = document.createElement("div");
+  newContainer.innerHTML = newShow;
+
+  // Append the new container element to the existing content
+  show.appendChild(newContainer);
 }
