71d903acb437eefb67c0487efbacb4a5ef1bba9c
iftekar2 <iftekar.hossain123@gmail.com>
2023-07-09 11:36:52 -0400

    Making Input boxes required before the form can be submited

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const submitButton = document.getElementById("button");
-
-submitButton.addEventListener("click", showUserInfo);
-
-function showUserInfo() {
-  // Get user input values
+function showUserInfo(event) {
+  event.preventDefault();
   const destination = document.getElementById("destination").value;
   const city = document.getElementById("city").value;
   const image = document.getElementById("image").value;
   const description = document.getElementById("description").value;
+
+  if (!destination || !city || !image || !description) {
+    return;
+  }
 
   showInfo(destination, city, image, description);
 }
@@ -15,38 +15,40 @@ function showUserInfo() {
 function showInfo(destination, city, image, description) {
   const show = document.getElementById("show");
   const newShow = `
-  <section class="showUserInputSection">
-    <div class="showUserInput">
-      <div class="destination">
-        <h3>Destination Name</h3>
-        <p id="destinationInput">${destination}</p>
-      </div>
-
-      <div class="city">
-        <h3>City Name</h3>
-        <p id="cityInput">${city}</p>
-      </div>
-
-      <div class="image">
-        <h3>Image</h3>
-        <div class="imageDiv">
-            <img id="imageInput" src="${image}"></img>
+    <section class="showUserInputSection">
+      <div class="showUserInput">
+        <div class="destination">
+          <h3>Destination Name</h3>
+          <p id="destinationInput">${destination}</p>
+        </div>
+  
+        <div class="city">
+          <h3>City Name</h3>
+          <p id="cityInput">${city}</p>
+        </div>
+  
+        <div class="image">
+          <h3>Image</h3>
+          <div class="imageDiv">
+              <img id="imageInput" src="${image}"></img>
+          </div>
+        </div>
+  
+        <div class="description">
+          <h3>City Description</h3>
+          <p id="descriptionInput">${description}</p>
+        </div>
+        <div class="removeButtonDiv">
+          <button class="removeButton" id="removeButton">Remove</button>
         </div>
       </div>
+    </section>`;
 
-      <div class="description">
-        <h3>City Description</h3>
-        <p id="descriptionInput">${description}</p>
-      </div>
-      <div class="removeButtonDiv">
-        <button class="removeButton" id="removeButton">Remove</button>
-      </div>
-    </div>
-  </section>`;
-  // Create a new container element
   const newContainer = document.createElement("div");
   newContainer.innerHTML = newShow;
 
-  // Append the new container element to the existing content
   show.appendChild(newContainer);
 }
+
+const submitButton = document.getElementById("button");
+submitButton.addEventListener("click", showUserInfo);
