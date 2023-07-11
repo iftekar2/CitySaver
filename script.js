fda8b338a3bd224e55ac70fd5709d3dd21d8af57
iftekar2 <iftekar.hossain123@gmail.com>
2023-07-10 21:38:51 -0400

    Final Commit

--- script.js
+++ script.js
@@ -2,22 +2,30 @@ const submitButton = document.getElementById("button");
 submitButton.addEventListener("click", showUserInfo);
 
 function showUserInfo(event) {
+  //This is preventing default
   event.preventDefault();
+
+  //This is getting value for destination, city, image, description.
   const destination = document.getElementById("destination").value;
   const city = document.getElementById("city").value;
   const image = document.getElementById("image").value;
   const description = document.getElementById("description").value;
 
+  //This makes sure that the user enteres something in destination, city, image, description.
+  //If they don't fill in all the boxes it would be submitted.
   if (!destination || !city || !image || !description) {
     return;
   }
 
+  //In here I am passing destination, city, image, description to showInfo Function.
   showInfo(destination, city, image, description);
 }
 
 function showInfo(destination, city, image, description) {
-  const show = document.getElementById("show");
-  const newShow = `
+  const showUserInputs = document.getElementById("showUserInputs");
+
+  //This is creating showUseInputSection every time the user clicks on Submit Button
+  const newShowUserInputs = `
     <section class="showUserInputSection">
       <div class="showUserInput">
         <div class="destination">
@@ -33,7 +41,7 @@ function showInfo(destination, city, image, description) {
         <div class="image">
           <h3>Image</h3>
           <div class="imageDiv">
-              <img id="imageInput" src="${image}"></img>
+            <img id="imageInput" src="${image}"></img>
           </div>
         </div>
   
@@ -48,11 +56,13 @@ function showInfo(destination, city, image, description) {
     </section>
   `;
 
+  //This allows us to create more than one User Input Section.
   const newContainer = document.createElement("div");
-  newContainer.innerHTML = newShow;
+  newContainer.innerHTML = newShowUserInputs;
 
-  show.appendChild(newContainer);
+  showUserInputs.appendChild(newContainer);
 
+  //This is Removing the Sections the user is creating.
   const removeSection = newContainer.querySelector(".removeButton");
   removeSection.addEventListener("click", () => {
     newContainer.remove();
